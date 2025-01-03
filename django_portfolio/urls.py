d2da9bf2c286c564b42bcaaf72015f5a5791c6be
benitovasquez <ricardobenitovasquezroca@gmail.com>
2025-01-03 11:01:06 -0400

    Fix templates and optional image handling

--- django_portfolio/urls.py
+++ django_portfolio/urls.py
@@ -20,5 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', include(('blog.urls', 'blog'), namespace='blog')),
     path('', include('portfolio.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
