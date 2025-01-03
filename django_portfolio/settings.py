245d6d0cd9000158d6217c0d54a268315075370d
benitovasquez <ricardobenitovasquezroca@gmail.com>
2025-01-03 11:16:51 -0400

    Fix templates and optional image handlin2g

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -134,8 +134,9 @@ STATIC_URL = '/static/'
 if not DEBUG:
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = "/public/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
--- django_portfolio/urls.py
+++ django_portfolio/urls.py
@@ -23,3 +23,5 @@ urlpatterns = [
     path('blog/', include(('blog.urls', 'blog'), namespace='blog')),
     path('', include('portfolio.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
