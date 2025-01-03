0f25c3462353901906ea315f53e5a0513d81890b
benitovasquez <ricardobenitovasquezroca@gmail.com>
2025-01-03 09:11:59 -0400

    2.1. Correcciones finales

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-5#lj2_sg0h4$ax&3lrbf3506b(9&@)hrb#12)q60(462*wlizz"
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tu-dominio.com', 'www.tu-dominio.com', 'localhost']
 
 
 # Application definition
@@ -127,6 +127,10 @@ USE_TZ = True
 STATIC_URL = "/static/"
 MEDIA_URL = "/public/"
 MEDIA_ROOT = BASE_DIR / "media"
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
