33af506cbfac1873999897b67109cd44e7b63665
benitovasquez <ricardobenitovasquezroca@gmail.com>
2025-01-04 08:09:30 -0400

    4.2.1 corrigiendo un poco

--- django_portfolio/wsgi.py
+++ django_portfolio/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-env = os.getenv('ENV', 'production')  # Por defecto, usa producción
+env = os.getenv('ENV', 'production')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'django_portfolio.settings.{env}')
 
 application = get_wsgi_application()
